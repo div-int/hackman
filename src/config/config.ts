cc3d9ff3838846508d2033ee5daf241d40fad2b4
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-07-17 00:19:23 +0100

    Add functionality to UIScene

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,17 +1,17 @@
 export default {
   type: Phaser.AUTO,
-  parent: "Phaser3",
+  parent: 'Phaser3',
   width: window.innerWidth,
   height: window.innerHeight,
-  backgroundColor: "#000000",
-  pixelArt: false,
-  roundPixels: false,
+  backgroundColor: '#000000',
+  pixelArt: true,
+  roundPixels: true,
   scale: {
     mode: Phaser.Scale.RESIZE,
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
       debug: false,
       gravity: { x: 0, y: 0 }
