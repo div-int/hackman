860c9e04a97b83dac3b8576cb719d1c4a52eab2a
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-07-17 23:43:44 +0100

    Add code to detect if on desktop/mobile

--- src/scenes/gamescene.ts
+++ src/scenes/gamescene.ts
@@ -6,15 +6,20 @@ import { GameState } from "./gamestate";
 import "../gameobjects/hackman";
 import { HackMan } from "../gameobjects/hackman";
 
-const MAXSPRITE = 500;
+const MAXSPRITEDESKTOP = 500;
+const MAXSPRITEMOBILE = 100;
+let maxsprite = 500;
 const WHITE = 0xffffff;
+
+let scale = 4;
+
 export class GameScene extends Phaser.Scene {
   private _uiscene: UIScene;
   private _levelscene: LevelScene;
   private _gameState: GameState;
 
   private _statusText: Phaser.GameObjects.BitmapText;
-  private _hackman: HackMan[] = new Array<HackMan>(MAXSPRITE);
+  private _hackman: HackMan[] = new Array<HackMan>(MAXSPRITEDESKTOP);
 
   constructor() {
     super("GameScene");
@@ -37,6 +42,14 @@ export class GameScene extends Phaser.Scene {
   create() {
     console.log(`GameScene::create() : ${Version}`);
 
+    if (this.sys.game.device.os.desktop) {
+      scale = 4;
+      maxsprite = MAXSPRITEDESKTOP;
+    } else {
+      scale = 2;
+      maxsprite = MAXSPRITEMOBILE;
+    }
+
     // Add UI scene object and start it.
     this.game.scene.add("UIScene", this._uiscene);
     this.game.scene.start("UIScene");
@@ -58,14 +71,14 @@ export class GameScene extends Phaser.Scene {
     // Add Level scene object but don't start it yet.
     // this.game.scene.add("LevelScene", this._levelscene);
 
-    for (let i = 0; i < this._hackman.length; i++) {
+    for (let i = 0; i < maxsprite; i++) {
       this._hackman[i] = new HackMan(this, 0, 0, 0);
       this._hackman[i].add(this);
     }
 
     this._hackman.map((hackman: HackMan) => {
       hackman
-        .setScale(4)
+        .setScale(scale)
         .setRandomPosition()
         .setCollideWorldBounds(true, 1, 1)
         .setVelocity(
@@ -79,7 +92,9 @@ export class GameScene extends Phaser.Scene {
   update(timestamp: number, elapsed: number) {
     if (this._statusText) {
       this._statusText.setText(
-        `${(timestamp / 1000.0).toFixed(0)}s ${elapsed.toFixed(2)}ms`
+        `${(timestamp / 1000.0).toFixed(0)}s ${elapsed.toFixed(2)}ms ${
+          this.sys.game.device.os.desktop ? "Desktop" : "Mobile"
+        }`
       );
     }
 
@@ -95,7 +110,7 @@ export class GameScene extends Phaser.Scene {
       hackman.setDepth(hackman.x + hackman.y * window.innerWidth);
     });
 
-    for (let i = 0; i < this._hackman.length >> 8; i++) {
+    for (let i = 0; i < maxsprite >> 8; i++) {
       let sprite: HackMan = this._hackman[
         Phaser.Math.Between(0, this._hackman.length - 1)
       ];
