23716b6a7900aec03a49c3fd61641e9e3c3dbc5b
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-07-18 22:06:30 +0100

    Add namespace and enums to store numbers and other static values

---
+++ src/consts/consts.ts
@@ -0,0 +1,19 @@
+namespace Consts {
+  export const enum MagicNumbers {
+    Zero = 0,
+    Tenth = 0.1,
+    Quarter = 0.25,
+    Half = 0.5,
+    One = 1,
+  }
+
+  export const enum Colours {
+    Off = 0,
+    On = 1,
+    Black = 0x000000,
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff,
+    White = 0xffffff,
+  }
+}
