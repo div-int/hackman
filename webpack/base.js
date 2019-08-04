022ae5cc120961243f25d07148a52a07178daa10
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-08-05 00:14:23 +0100

    Fix double quotes

--- webpack/base.js
+++ webpack/base.js
@@ -1,57 +1,56 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const GitRevisionPlugin = require("git-revision-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
 const gitRevisionPlugin = new GitRevisionPlugin({
   lightweightTags: true,
 });
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.ts",
-  devtool: "inline-source-map",
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    path: path.resolve(__dirname, "../dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.ts$/,
-        loader: "ts-loader",
-        exclude: "/node_modules/",
+        loader: 'ts-loader',
+        exclude: '/node_modules/',
       },
       {
         test: /\.js$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.(gif|png|jpe?g|svg|xml)$/i,
-        use: "file-loader",
+        use: 'file-loader',
       },
     ],
   },
   devServer: {
-    contentBase: path.resolve(__dirname, "../src/"),
-    publicPath: "/",
-    host: "localhost",
+    contentBase: path.resolve(__dirname, '../src/'),
+    publicPath: '/',
+    host: 'localhost',
     port: 8080,
     open: true,
   },
   plugins: [
-    new CleanWebpackPlugin(["dist"], {
-      root: path.resolve(__dirname, "../"),
+    new CleanWebpackPlugin(['dist'], {
+      root: path.resolve(__dirname, '../'),
     }),
     new webpack.DefinePlugin({
       CANVAS_RENDERER: JSON.stringify(true),
       WEBGL_RENDERER: JSON.stringify(true),
     }),
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: './index.html',
     }),
     gitRevisionPlugin,
     new webpack.DefinePlugin({
--- webpack/prod.js
+++ webpack/prod.js
@@ -1,17 +1,17 @@
-const merge = require("webpack-merge");
-const path = require("path");
-const base = require("./base");
-const TerserPlugin = require("terser-webpack-plugin");
+const merge = require('webpack-merge');
+const path = require('path');
+const base = require('./base');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(base, {
-  mode: "production",
+  mode: 'production',
   output: {
-    filename: "bundle.min.js",
+    filename: 'bundle.min.js',
   },
   devtool: false,
   performance: {
-    maxEntrypointSize: 900000,
-    maxAssetSize: 900000,
+    maxEntrypointSize: 2000000,
+    maxAssetSize: 2000000,
   },
   optimization: {
     minimizer: [
